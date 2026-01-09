12ac3bd7143042be4706cd5cf33e7c338a82fbd0
Laxman <laxmanpradhan784@gmail.com>
2026-01-09 17:00:00 +0530

    new changes

---
+++ resources/views/adminside/layouts/auth.blade.php
@@ -0,0 +1,79 @@
+<!doctype html>
+<html class="no-js" lang="en">
+
+
+<!-- Mirrored from new.axilthemes.com/demo/template/etrade/index-1.html by HTTrack Website Copier/3.x [XR&CO'2014], Tue, 06 Jan 2026 12:12:29 GMT -->
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>E-Trade</title>
+    <meta name="robots" content="noindex, follow" />
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- Favicon -->
+   <link rel="shortcut icon" type="image/x-icon" href="{{ asset('assets/images/favicon.png') }}">
+
+
+    <!-- CSS
+    ============================================ -->
+
+
+    <!-- Bootstrap CSS -->
+<link rel="stylesheet" href="{{ asset('assets/css/vendor/bootstrap.min.css') }}">
+<link rel="stylesheet" href="{{ asset('assets/css/vendor/font-awesome.css') }}">
+<link rel="stylesheet" href="{{ asset('assets/css/vendor/flaticon/flaticon.css') }}">
+<link rel="stylesheet" href="{{ asset('assets/css/vendor/slick.css') }}">
+<link rel="stylesheet" href="{{ asset('assets/css/vendor/slick-theme.css') }}">
+<link rel="stylesheet" href="{{ asset('assets/css/vendor/jquery-ui.min.css') }}">
+<link rel="stylesheet" href="{{ asset('assets/css/vendor/sal.css') }}">
+<link rel="stylesheet" href="{{ asset('assets/css/vendor/magnific-popup.css') }}">
+<link rel="stylesheet" href="{{ asset('assets/css/vendor/base.css') }}">
+<link rel="stylesheet" href="{{ asset('assets/css/style.min.css') }}">
+
+
+</head>
+
+
+<body class="sticky-header newsletter-popup-modal">
+
+<div class="container">
+    @yield('content')
+</div>
+
+<!-- Offer Modal End -->
+    <!-- JS
+============================================ -->
+<!-- Modernizer JS -->
+<script src="{{ asset('assets/js/vendor/modernizr.min.js') }}"></script>
+
+<!-- jQuery JS -->
+<script src="{{ asset('assets/js/vendor/jquery.js') }}"></script>
+
+<!-- Bootstrap JS -->
+<script src="{{ asset('assets/js/vendor/popper.min.js') }}"></script>
+<script src="{{ asset('assets/js/vendor/bootstrap.min.js') }}"></script>
+
+<script src="{{ asset('assets/js/vendor/slick.min.js') }}"></script>
+<script src="{{ asset('assets/js/vendor/js.cookie.js') }}"></script>
+
+{{-- <script src="{{ asset('assets/js/vendor/jquery.style.switcher.js') }}"></script> --}}
+
+<script src="{{ asset('assets/js/vendor/jquery-ui.min.js') }}"></script>
+<script src="{{ asset('assets/js/vendor/jquery.ui.touch-punch.min.js') }}"></script>
+<script src="{{ asset('assets/js/vendor/jquery.countdown.min.js') }}"></script>
+<script src="{{ asset('assets/js/vendor/sal.js') }}"></script>
+<script src="{{ asset('assets/js/vendor/jquery.magnific-popup.min.js') }}"></script>
+<script src="{{ asset('assets/js/vendor/imagesloaded.pkgd.min.js') }}"></script>
+<script src="{{ asset('assets/js/vendor/isotope.pkgd.min.js') }}"></script>
+<script src="{{ asset('assets/js/vendor/counterup.js') }}"></script>
+<script src="{{ asset('assets/js/vendor/waypoints.min.js') }}"></script>
+
+<!-- Main JS -->
+<script src="{{ asset('assets/js/main.js') }}"></script>
+
+
+</body>
+
+
+<!-- Mirrored from new.axilthemes.com/demo/template/etrade/index-1.html by HTTrack Website Copier/3.x [XR&CO'2014], Tue, 06 Jan 2026 12:12:43 GMT -->
+</html>
